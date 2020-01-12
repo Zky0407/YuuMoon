cc40db9ee051e3efcfb9404e8e0616d92f27ca39
yuumoon <yuumoon_com@foxmail.com>
2020-01-12 19:57:11 +0800

    更改项目名称 + 重新创建虚拟环境

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\zky04\downloads\django2.0-course-master\django2.0-course-master\34.评论发送邮件通知\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
